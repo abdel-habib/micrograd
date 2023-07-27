82dc01afc4670d527926069264cab0c7f1cbb6d8
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-07-27 15:15:46 +0300

    decoupled files

---
+++ micrograd/engine.py
@@ -0,0 +1,27 @@
+import math
+
+class Value():
+    """Stores a single scalar value and its gradient"""
+    def __init__(self, data, _children=(), _op='', label=''):
+        self.data = data
+        self.grad = 0.0
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+        
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+    
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+        return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+        return out
