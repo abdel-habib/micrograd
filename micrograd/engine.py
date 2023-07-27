751a1c2836842a13aafae9cfbf658bf62ca8d1ed
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-07-27 16:03:33 +0300

    finalized backpropagation function

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -5,6 +5,7 @@ class Value():
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -14,14 +15,48 @@ class Value():
     
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
+        
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+
         return out
     
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        
         return out
     
     def tanh(self):
         x = self.data
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
         out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+
         return out
+    
+    def backward(self):
+        # using graphs topological sort to build a topological graph
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self) 
+
+        self.grad = 1.0
+        # now we can call the backward method on all of the nodes of the topological order
+        for node in reversed(topo):
+            node._backward()
