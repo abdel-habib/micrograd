71500dc4c4b0b25383d50dbb72b2acbd9b40a0a8
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-07-28 14:14:26 +0300

    adding more functions to handle tanh activation function

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -14,6 +14,7 @@ class Value():
         return f"Value(data={self.data})"
     
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
         
         def _backward():
@@ -24,6 +25,7 @@ class Value():
         return out
     
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
         
         def _backward():
@@ -33,6 +35,38 @@ class Value():
         
         return out
     
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+        out = Value(self.data**other, (self, ), f'**{other}')
+
+        def _backward():
+            self.grad += other * (self.data**(other - 1)) * out.grad
+        out._backward = _backward
+
+        return out
+    
+    def __rmul__(self, other): # other * self
+        return self * other
+    
+    def __truediv__(self, other): # self / other
+        return self * other**-1
+    
+    def __neg__(self): # -self
+        return self * -1
+    
+    def __sub__(self, other): # self - other
+        return self + (-other)
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self, ), 'exp')
+
+        def _backward():
+            self.grad += out.data * out.grad
+        out._backward = _backward
+
+        return out
+
     def tanh(self):
         x = self.data
         t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
